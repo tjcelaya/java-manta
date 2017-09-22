eea0962de8ea62b30dbc1ef659e9510ac2330aea
Tomas Celaya <tjcelaya@gmail.com>
2017-09-21 17:53:21 -0700

    Extract request-building convenience methods into a new MantaHttpRequestFactory class

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/http/MantaApacheHttpClientContext.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/http/MantaApacheHttpClientContext.java
@@ -39,6 +39,17 @@ public class MantaApacheHttpClientContext implements MantaConnectionContext {
         this.httpClient = connectionFactory.createConnection();
     }
 
+    /**
+     * Creates a new instance using the provided client connection.
+     * @param httpClient factory class that creates configured connections
+     */
+    public MantaApacheHttpClientContext(final CloseableHttpClient httpClient) {
+        Validate.notNull(httpClient,
+                "Client must not be null");
+
+        this.httpClient = httpClient;
+    }
+
     @Override
     public CloseableHttpClient getHttpClient() {
         return httpClient;
